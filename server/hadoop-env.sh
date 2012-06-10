c0c04d9609f2c4a57a86452aa1527b822fdfc44d
shingo furuyama <marblejenka@gmail.com>
2012-06-10 15:33:19 +0900

    ログの区切り文字を指定しないよう修正

--- server/hadoop-env.sh
+++ server/hadoop-env.sh
@@ -39,4 +39,4 @@ export HADOOP_PID_DIR=/var/local/hadoop/pid
 export HADOOP_SECURE_DN_PID_DIR=$HADOOP_PID_DIR
 
 # A string representing this instance of hadoop. $USER by default.
-export HADOOP_IDENT_STRING=$USER
+# export HADOOP_IDENT_STRING=$USER
